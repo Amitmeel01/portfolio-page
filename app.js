fe86045939a8b42b42d29064f5835bc901a9ff6b
Amitmeel01 <meelamit354@gmail.com>
2024-08-10 00:08:00 +0530

    update

--- app.js
+++ app.js
@@ -5,10 +5,10 @@
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menu.addEventListener("click", () => {
-  // menu.classList.toggle('bx-x');
-  navbar.classList.toggle("active");
-});
+// menu.addEventListener("click", () => {
+//   // menu.classList.toggle('bx-x');
+//   navbar.classList.toggle("active");
+// });
 
 //scroll section
 
@@ -237,3 +237,33 @@ form.addEventListener("submit", (e) => {
   sent();
   resetForm();
 });
+
+
+
+//sidebar
+
+
+let sider = document.querySelector(".sidebar");
+let icon = document.querySelectorAll(".sidebar a");
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  sider.style.display = "none";
+}
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "400px";
+  sider.style.display = "block";
+}
+
+icon.forEach((item) => {
+  item.addEventListener("click", ()=> {
+    closeNav(); // Close the sidebar when a link is clicked
+    // event.preventDefault(); // Prevent default link behavior
+    let targetId = this.getAttribute("href"); // Get the target section ID
+    let targetElement = document.querySelector(targetId); // Find the target element
+    targetElement.scrollIntoView({ behavior: "smooth" }); // Scroll to the target element
+  });
+});
+
+
